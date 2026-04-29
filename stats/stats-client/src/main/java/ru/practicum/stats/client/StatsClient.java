03e61bacbb189c74b570dc44b22d2979e974938e
Olga Zavalishina <steffane@yandex.ru>
2026-04-29 20:05:31 +0300

    diplom_project_part1_ver_1.0

---
+++ stats/stats-client/src/main/java/ru/practicum/stats/client/StatsClient.java
@@ -0,0 +1,33 @@
+package ru.practicum.stats.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.stats.dto.EndpointHitDto;
+
+@Service
+public class StatsClient extends BaseClient {
+
+    @Autowired
+    public StatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                        .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> hit(EndpointHitDto hitDto) {
+        return post("/hit", hitDto);
+    }
+
+    public ResponseEntity<Object> getStats(String start, String end, String uris, Boolean unique) {
+        String path = String.format("/stats?start=%s&end=%s&uris=%s&unique=%s", start, end, uris, unique);
+        return get(path);
+    }
+}
---
+++ stats/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
@@ -0,0 +1,31 @@
+package ru.practicum.stats.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EndpointHitDto {
+
+    private Long id;
+
+    @NotBlank
+    private String app;
+
+    @NotBlank
+    private String uri;
+
+    @NotBlank
+    private String ip;
+
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
