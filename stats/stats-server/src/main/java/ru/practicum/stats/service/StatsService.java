e6192407da3d5062f2bde5beeb167c3ed5a43977
Olga Zavalishina <steffane@yandex.ru>
2026-04-29 20:31:12 +0300

    diplom_project_part1_ver_1.2 checkstyle

--- stats/stats-server/src/main/java/ru/practicum/stats/service/StatsService.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/service/StatsService.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 public interface StatsService {
     void saveHit(EndpointHitDto hitDto);
+
     List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique);
 }
